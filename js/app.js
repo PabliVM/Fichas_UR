94b5f915116d687586a07785124317d3b234de20
PabliVM <pablo.vegamartin@gmail.com>
2026-09-26 15:01:54 +0200

    Fix formatting issues in app.js

--- js/app.js
+++ js/app.js
@@ -657,10 +657,10 @@ function buildFichaDemoFromSchema(positionKey) {
       condicional: { rp: null,         items: condicional },
     },
     plan: {
-      tecnico:     ['', '', '', '', '', '', '', ''],
-      tactico:     ['', '', '', '', '', '', '', ''],
-      condicional: ['', '', '', '', '', '', '', ''],
-      mental:      ['', '', '', '', '', '', '', ''],
+      tecnico:     ['', '', '', '', '', '', ''],
+      tactico:     ['', '', '', '', '', '', ''],
+      condicional: ['', '', '', '', '', '', ''],
+      mental:      ['', '', '', '', '', '', ''],
     },
   };
 }
